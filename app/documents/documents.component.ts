eaa80e92ea344f212e9f9ec5dc47bc3a7f36a9d2
Trevor <trevorhoward2@hotmail.com>
2017-06-07 14:34:03 -0600

    Added cards to documents page

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
 			description: "Top level clearance needed",
 			file_url: 'http://www.google.com',
 			updated_at: 'just now',
-			image_url: 'http://www.google.com'
+			image_url: 'http://www.dmv.gov.za/img/documents.jpg'
 		},
 		{
 			title: "Secret File 2 - Confidential",
 			description: "Top level clearance needed",
 			file_url: 'http://www.google.com',
 			updated_at: 'just now',
-			image_url: 'http://www.google.com'
+			image_url: 'http://greenapplemortgage.com/wp-content/uploads/2015/12/Documents.jpg'
 		},
 		{
 			title: "Secret File 3 - Confidential",
 			description: "Top level clearance needed",
 			file_url: 'http://www.google.com',
 			updated_at: 'just now',
-			image_url: 'http://www.google.com'
+			image_url: 'http://printeger.eu/wp-content/uploads/2016/03/iStock_000028512836_Full_MIT-cOPYRIGHT.jpg'
 		}
 	]
 }
